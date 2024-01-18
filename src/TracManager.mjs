07817ab3114b30e926b554e2b4c05818ae108dcc
eL1x00r <elixtrac@icloud.com>
2024-01-18 11:35:19 +0100

    more wrapping into class structure

--- src/TracManager.mjs
+++ src/TracManager.mjs
@@ -1,20 +1,34 @@
 import Corestore from "corestore";
 import Hyperswarm from "hyperswarm";
 import Hyperbee from "hyperbee";
+import goodbye from "graceful-goodbye";
+
 // import WebsocketModule from './WebsocketModule
 export default class TracManager {
     constructor() {
         this.store = new Corestore();
         this.swarm = new Hyperswarm();
         this.bee = null;
+
+        goodbye(function () {
+            this.swarm.destroy();
+        });
     }
 
     async initReader(coreSetup, server=true, client=true, rangeStart=-1, rangeEnd=-1) {
         // Initialize Corestore and Hyperswarm
         this.initCorestore(coreSetup);
         this.initHyperswarm(server, client);
+
         // Additional setup steps...
         await this.startRangeDownload(rangeStart, rangeEnd);
+        
+        this.bee = new Hyperbee(this.core, {
+            keyEncoding: "utf-8",
+            valueEncoding: "utf-8",
+        });
+    
+        await this.sleep(30 * 1000);
     }
 
     async initCorestore(coreSetup) {
@@ -44,7 +58,35 @@ export default class TracManager {
     }
 
     async startRangeDownload(start, end) {
-        // Logic for rangeDownload from createTrac
+        console.log("Starting chunk download. Core length:", this.core.length);
+
+        if (end < 0) {
+          end = this.core.length;
+        }
+    
+        let chunk_size = 20000;
+    
+        for (let i = start; i < end; i++) {
+          console.log("Next chunk", i, i + chunk_size);
+          const range = this.core.download({ start: i, end: i + chunk_size });
+          await range.done();
+          i = i + chunk_size - 1;
+          start = i;
+        }
+    
+        if (end == -1) {
+          const discovery = this.swarm.refresh({ server: true, client: true }); // hardcoded for now, does this need to be configurable?
+          await discovery.flushed();
+          const foundPeers = this.store.findingPeers();
+          await this.swarm.flush();
+          await foundPeers();
+          await this.sleep(1000);
+    
+          this.startRangeDownload(start, end);
+        }
     }
 
+    sleep(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
 }
