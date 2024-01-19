1768375590d9f60fe85a7f080f76d83701e2f943
eL1x00r <elixtrac@icloud.com>
2024-01-19 13:06:50 +0100

    raw function call to tapProtocol method succeeded

--- src/main.mjs
+++ src/main.mjs
@@ -1,4 +1,6 @@
 import TracManager from "./TracManager.mjs";
 
-let xoxo = new TracManager();
-let test = xoxo.initReader("729c91276e20b8e270ea589ac437f24e6c7c66c969b4acfe99bd82faab391e68",true,true,-1,-1)
+let tracCore = new TracManager();
+await tracCore.initReader("729c91276e20b8e270ea589ac437f24e6c7c66c969b4acfe99bd82faab391e68",true,true,-1,-1)
+
+console.log( await tracCore.tapProtocol.getTransferAmountByInscription('1b8e21761557bbf66c06ae3d8109764d0d8ec5d431b8291160b59ef28ffaab7ai0'))
