029c23975ec545edc91ce2bd3ee2ce2dbd48b2c2
eL1x00r <elixtrac@icloud.com>
2024-01-19 09:41:27 +0100

    added tracManager param in constructor, added cmd validation

--- src/WebsocketModule.mjs
+++ src/WebsocketModule.mjs
@@ -3,12 +3,14 @@ import { Server } from "socket.io";
 import config from "config";
 
 export default class WebsocketModule{
-    constructor(){
-      console.log("Starting socket.io");
+    constructor(tracManager){
+      this.tracManager = tracManager;
+      
       this.socket_port = config.get("websocketPort");
       this.httpServer = createServer();
       this.httpServer.maxConnections = 1000;
-
+      
+      console.log("Starting socket.io");
       this.io = new Server(this.httpServer, {
         cors: {
           origin: config.get("websocketCORS"),
@@ -17,8 +19,26 @@ export default class WebsocketModule{
     
       this.io.on("connection", (socket) => {
         socket.on("get", async (cmd) => {
+          if (!this.validCmd(cmd, socket)) return;
           
         })
       })
   }
+  invalidCmd(cmd, socket) {
+    this.io.to(socket.id).emit("error", { error: "invalid command", cmd: cmd });
+  }
+  validCmd(cmd, socket) {
+    if (
+      typeof cmd.call_id == "undefined" ||
+      typeof cmd.func == "undefined" ||
+      typeof cmd.args == "undefined" ||
+      !Array.isArray(cmd.args)
+    ) {
+      this.invalidCmd(cmd, socket);
+      return false;
+    }
+  
+    return true;
+  }
+  
 }
